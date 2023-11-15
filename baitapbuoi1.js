9f7832a0eeb9386d6463e0b2fa766b02752ef993
TheManh2004 <themanh20004@gmail.com>
2023-11-15 09:16:56 +0700

    first commit

---
+++ baitapbuoi1.js
@@ -0,0 +1,72 @@
+//vietchuong trình javascrips kiểm tra số nguyên là dương hay là âm
+// const a = 5;
+// if(a>0){
+//     console.log("số dương")
+// }
+// else if(a=0){
+//     console.log("0")
+// }
+// else{
+//     console.log("số âm")
+// }
+//Tạo một biến age và kiểm tra xem age có lớn hơn hoặc bằng 18 không. Nếu có, in ra "Bạn đã đủ tuổi"; ngược lại, in ra "Bạn chưa đủ tuổi".
+// var age=18;
+// if(age>=18){
+//     console.log("bạn đã đủ tuổi")
+// }
+// else{
+//     console.log("bạn chưa đủ tuổi")
+// }
+// Viết chương trình JavaScript để kiểm tra xem một chuỗi có độ dài lớn hơn 5 ký tự không. Nếu có, in ra "Chuỗi quá dài"; ngược lại, in ra "Chuỗi đủ ngắn".
+// const chuoi="Javascript"
+// độ dài chuỗi dùng cú pháp teenkitu.length
+// if(chuoi.length>5){
+//     console.log("Chuỗi quá dài")
+// }
+// else{
+//     console.log("Chuỗi vừa ngắn")
+// }
+// Tạo một biến score và kiểm tra điểm số của học sinh theo hệ điểm 10. Nếu score lớn hơn hoặc bằng 5, in ra "Đậu"; ngược lại, in ra "Trượt".
+//  const score=7;
+//  if(score>=5){
+//      console.log("Đậu")
+//     }
+//  else{
+//     console.log("Trượt")
+//  }
+//Viết chương trình JavaScript để kiểm tra xem một số nguyên có phải là số chẵn hay không. Nếu là số chẵn, in ra "Số chẵn"; ngược lại, in ra "Số lẻ".
+//  const a = 5;
+//  if(a%2==1){
+//      console.log("số lẻ")
+//     }
+//  else{
+//      console.log("số chẵn")
+//      }
+//Tạo một biến hour và kiểm tra xem hour có phải là giờ hợp lệ (0-23) không.
+//   const hour=24;
+//   if(hour>0 && hour<23){
+//       console.log("hợp lệ")
+//      }
+//   else{
+//      console.log("không hợp lệ")
+//   }
+//Viết chương trình JavaScript để tính lãi suất đơn giản cho một số tiền gửi dựa trên số tiền, lãi suất và thời gian gửi.
+// const sotien=1000000;
+// const laisuatduoi12thang=0.05;
+// const laisuattren12thang=0.07;
+// const time=13;
+// if(time>12){
+//     console.log("Số tiền lãi",sotien*laisuattren12thang)
+// }
+// else{
+//     console.log("số tiền lãi",sotien*laisuatduoi12thang)
+// }
+//Tạo một biến word và kiểm tra xem word có phải là từ viết tắt (ví dụ: "ASAP", "LOL") không.
+const word1="Liên minh huyền thoại"
+const word="LOL"
+if(word=="LOL"){
+    console.log("word là từ viết tắt")
+}
+else{
+    console.log("Word không phải là từ viết tắt")
+}
---
+++ buoi1.js
@@ -0,0 +1,19 @@
+
+const a = 5;
+if(a>0){
+    console.log("số dương")
+}
+else if(a=0){
+    console.log("0")
+}
+else{
+    console.log("số âm")
+}
+
+var age=18;
+if(age>=18){
+    console.log("bạn đã đủ tuổi")
+}
+else{
+    console.log("bạn chưa đủ tuổi")
+}
